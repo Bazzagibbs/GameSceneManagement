e9e1767c813e420f659f1f6b1f7cc23dd4650df3
Bazzagibbs <bailey.gibbons2018@gmail.com>
2023-04-30 19:53:40 +1000

    Added utility menu item: Tools > Game Scene Manager > Play Mode Launch Settings. Finds existing launch settings asset, or creates one if none exist.

--- Editor/Scripts/PlayModeLaunchSettings.cs
+++ Editor/Scripts/PlayModeLaunchSettings.cs
@@ -9,9 +9,33 @@ namespace BazzaGibbs.GameSceneManager {
     [CreateAssetMenu(menuName = "Game Scene Manager/Play Mode Launch Settings", fileName = "PlayModeLaunchSettings", order = 22)]
     public class PlayModeLaunchSettings : ScriptableObject {
         public bool enabled = true;
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
         public SceneAsset playModeScene;
-        #endif
+
+        [MenuItem("Tools/Game Scene Manager/Play Mode Launch Settings")]
+        public static void FindPlayModeLaunchSettings() {
+            
+            string[] results = AssetDatabase.FindAssets("t:PlayModeLaunchSettings");
+            PlayModeLaunchSettings instance;
+        
+            if (results.Length <= 0) {
+                instance = CreateInstance<PlayModeLaunchSettings>();
+                AssetDatabase.CreateAsset(instance, "Assets/Settings/PlayModeLaunchSettings.asset");
+            } 
+            else {
+                if (results.Length > 1) {
+                    Debug.LogWarning("[Game Scene Manager] Multiple PlayModeLaunchSettings assets found. Please make sure there are no more than one.");
+                }
+
+                string path = AssetDatabase.GUIDToAssetPath(results[0]);
+                instance = AssetDatabase.LoadAssetAtPath<PlayModeLaunchSettings>(path);
+            }
+            
+            Selection.activeObject = instance;
+            EditorGUIUtility.PingObject(instance);
+        }
+        
+#endif
         private void Awake() {
             SetPlayModeScene();
         }
@@ -31,4 +55,5 @@ namespace BazzaGibbs.GameSceneManager {
 #endif
         }
     }
+    
 }
